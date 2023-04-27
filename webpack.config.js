c995012586c1d07ee0e3b908a1f816280e04e563
kennankole <kennankole@gmail.com>
2023-04-27 14:21:09 +0300

    Setting up local development server in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -18,6 +25,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
