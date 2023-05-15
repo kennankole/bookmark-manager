cd2422596467f04018923d937f86609b5ad9e56b
kennankole <kennankole@gmail.com>
2023-05-15 19:49:54 +0300

    Fetching all the bookmarks

--- src/modules/chromeTab.js
+++ src/modules/chromeTab.js
@@ -33,21 +33,47 @@ if (savedUrls) {
   display();
 }
 
-chrome.bookmarks.getTree((bookmarkTreeNodes) => {
-  const bookmarkFolders = [];
+// const processNode = (node) => {
+//   if (node.children) {
+//     node.children.forEach((child) => {
+//       processNode(child);
+//     });
+//   }
+//   if (node.url) {
+//     console.log(node.url);
+//   }
+// };
 
-  const findFolders = (node) => {
-    if (node.children) {
-      node.children.forEach((childNode) => {
-        if (childNode.children) {
-          bookmarkFolders.push(childNode);
-          findFolders(childNode);
-        }
-      });
-    }
-  };
+// chrome.bookmarks.getTree((itemTree) => {
+//   itemTree.forEach((item) => {
+//     processNode(item);
+//   });
+// });
+
+const bookmarkFolders = [];
+
+const findFolders = (node) => {
+  if (node.children) {
+    node.children.forEach((childNode) => {
+      if (childNode.children) {
+        bookmarkFolders.push(childNode);
+        localStorage.setItem('allBookmarks', JSON.stringify(bookmarkFolders));
+        findFolders(childNode);
+      }
+    });
+  }
+};
+
+chrome.bookmarks.getTree((bookmarkTreeNodes) => {
   bookmarkTreeNodes.forEach((node) => {
     findFolders(node);
   });
-  console.log(bookmarkFolders);
+  const allBookmarks = JSON.parse(localStorage.getItem('allBookmarks'));
+  allBookmarks.forEach((item, index) => {
+    if (item.children.length > 1) {
+      console.log(item.children, item.title);
+    } else {
+      console.log(item.children[0].url, item.title);
+    }
+  });
 });
