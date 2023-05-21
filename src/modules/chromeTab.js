ba38389caea5f3486ae7624091d23da28838176c
kennankole <kennankole@gmail.com>
2023-05-21 19:35:46 +0300

    Implementing autocomplete Bookmark search.

--- src/modules/chromeTab.js
+++ src/modules/chromeTab.js
@@ -1,76 +1,45 @@
 /* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-const savedUrls = JSON.parse(localStorage.getItem('bookmarks')) || [];
-const bookMarkUrls = document.getElementById('bookmark-elements');
-const saveBtn = document.getElementById('save-btn');
-const inputElement = document.getElementById('current-bookmark');
 
-const display = () => {
-  bookMarkUrls.innerHTML = '';
-  savedUrls.forEach((item) => {
-    bookMarkUrls.innerHTML += `
-      <li>
-        <a href="${item}" target="_blank">${item}</a>
-      </li>
-    `;
+let allBookmarksData = [];
+
+const getAllBookmarks = (bookmakrs) => {
+  const allBookmarks = [];
+  bookmakrs.forEach((bookmark) => {
+    if (bookmark.url) {
+      allBookmarks.push({
+        title: bookmark.title,
+        url: bookmark.url,
+      });
+    } else if (bookmark.children) {
+      const childBookmars = getAllBookmarks(bookmark.children);
+      allBookmarks.push(...childBookmars);
+    }
+  });
+  return allBookmarks;
+};
+
+const updateAutocompleteDropdown = (matchingBookmarks) => {
+  matchingBookmarks.forEach((child) => {
+    console.log(child.url, child.title);
   });
 };
 
-saveBtn.addEventListener('click', (event) => {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  }, (tabs) => {
-    const currentTab = tabs[0].url;
-    savedUrls.push(currentTab);
-    localStorage.setItem('bookmarks', JSON.stringify(savedUrls));
-    display();
+const autocompleteSearch = (query) => {
+  const matchingBookmarks = allBookmarksData.filter((bookmark) => {
+    const title = bookmark.title.toLowerCase();
+    const queryLowerCase = query.toLowerCase();
+    return title.includes(queryLowerCase);
   });
-});
 
-if (savedUrls) {
-  display();
-}
-
-const options = {
-  includeScore: true,
-  keys: [
-    'url',
-    'title',
-    'children',
-  ],
+  updateAutocompleteDropdown(matchingBookmarks);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.getElementById('searchInput');
-  const searchButton = document.getElementById('searchButton');
-  const resultsList = document.getElementById('resultsList');
-
-  searchButton.addEventListener('click', () => {
-    const query = searchInput.value;
-
-    chrome.bookmarks.search(query, (result) => {
-      const fuse = new Fuse(result, options);
-      const results = fuse.search(query);
-      // eslint-disable-next-line no-console
-      console.log(results);
-    });
-    // chrome.bookmarks.search(query, (results) => {
-    //   resultsList.innerHTML = '';
-
-    //   if (results.length === 0) {
-    //     resultsList.innerHTML = '<li>No bookmarks found.</li>';
-    //   } else {
-    //     results.forEach((result) => {
-    //       const listItem = document.createElement('li');
-    //       listItem.textContent = result.url;
-    //       resultsList.appendChild(listItem);
-    //     });
-    //   }
-    // });
-  });
+const searchInput = document.getElementById('input');
+searchInput.addEventListener('input', (event) => {
+  const query = event.target.value;
+  autocompleteSearch(query);
 });
 
-const one = new Fuse();
-// eslint-disable-next-line no-console
-console.log(one);
+chrome.bookmarks.getTree((bookmarks) => {
+  allBookmarksData = getAllBookmarks(bookmarks);
+});
